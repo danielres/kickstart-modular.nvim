0540f8b9f0cceb816a333e41be8cf9fb6ebc2a4b
Daniel Reszka <dannn.r@gmail.com>
2026-01-18 15:54:53 +0100

    nordic theme: darken "black0"

--- lua/custom/plugins/nordic.lua
+++ lua/custom/plugins/nordic.lua
@@ -6,7 +6,7 @@ return {
     -- require('nordic').load()
     require('nordic').setup {
       on_palette = function(palette)
-        palette.black0 = '#121419'
+        palette.black0 = '#0d0f14'
         palette.gray0 = '#191D24'
       end,
       integrations = {
