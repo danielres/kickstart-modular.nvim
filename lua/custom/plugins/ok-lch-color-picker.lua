a46b3c40566b247a3ebb1c2eac698fc59e029c84
Daniel Reszka <dannn.r@gmail.com>
2026-01-18 15:21:38 +0100

    ok-lch-color-picker: add

---
+++ lua/custom/plugins/ok-lch-color-picker.lua
@@ -0,0 +1,25 @@
+return {
+  'eero-lehtinen/oklch-color-picker.nvim',
+  event = 'VeryLazy',
+  version = '*',
+  keys = {
+    -- One handed keymap recommended, you will be using the mouse
+    {
+      '<leader>v',
+      function()
+        require('oklch-color-picker').pick_under_cursor()
+      end,
+      desc = 'Color pick under cursor',
+    },
+  },
+  opts = {
+    -- Note: requires separate binary, if the auto_download one doesn't work on nixos:
+    -- 1) Install `oklch-color-picker` on nixos
+    -- 2) Replace:
+    --     ~/.local/share/vim-kickstart-modular/oklch-color-picker/oklch-color-picker
+    --   with symlink to:
+    --     ~/.nix-profile/bin/oklch-color-picker
+
+    auto_download = false,
+  },
+}
