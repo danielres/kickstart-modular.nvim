ba1b1308fb173d7d6ca54d0f6c93478d5ec186d7
Daniel Reszka <dannn.r@gmail.com>
2026-01-21 10:02:12 +0100

    snacks: enable toggle +  add mappings

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,5 +1,7 @@
 return {
   'folke/snacks.nvim',
+  lazy = false,
+  priority = 1000,
   opts = {
     styles = {
       -- use entire screen space
@@ -11,7 +13,28 @@ return {
       configure = true,
       theme_path = vim.fs.normalize(vim.fn.stdpath 'cache' .. '/lazygit-theme.yml'),
     },
+    toggle = {},
   },
+  init = function()
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VeryLazy',
+      callback = function()
+        -- Create toggle mappings
+        Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
+        Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
+        Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>uL'
+        Snacks.toggle.diagnostics():map '<leader>ud'
+        Snacks.toggle.line_number():map '<leader>ul'
+        Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>uc'
+        Snacks.toggle.treesitter():map '<leader>uT'
+        Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
+        Snacks.toggle.inlay_hints():map '<leader>uh'
+        Snacks.toggle.indent():map '<leader>ug'
+        Snacks.toggle.dim():map '<leader>uD'
+        Snacks.toggle.zoom():map '<leader>uz'
+      end,
+    })
+  end,
   keys = {
     {
       '<leader>ba',
