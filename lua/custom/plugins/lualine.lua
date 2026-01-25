dbe2ab8bf2048564c50dae47de7c396d1e973774
Daniel Reszka <dannn.r@gmail.com>
2026-01-25 08:15:12 +0100

    lualine: add macro recording feedback component

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,6 +2,28 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup {}
+    require('lualine').setup {
+      sections = {
+        lualine_a = {
+          -- Custom component for macro recording feedback
+          {
+            function()
+              local reg = vim.fn.reg_recording()
+              if reg ~= '' then
+                return '@' .. reg
+              end
+              return ''
+            end,
+            color = { fg = '#ffffff', bg = '#ff0000' }, -- White text on red background
+          },
+          'mode',
+        },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    }
   end,
 }
