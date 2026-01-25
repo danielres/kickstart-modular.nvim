46743a1c5bb460854f9d6c3d8d50ef30c548d797
Daniel Reszka <dannn.r@gmail.com>
2026-01-25 16:06:50 +0100

    obsidian.nvim: add + setup

--- lua/custom/autocmd.lua
+++ lua/custom/autocmd.lua
@@ -3,3 +3,10 @@ vim.api.nvim_create_autocmd(
   { 'BufLeave', 'FocusLost', 'ModeChanged' },
   { desc = 'autosave', pattern = '*', command = 'silent! update' }
 )
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    vim.opt_local.conceallevel = 2
+  end,
+})
