9f28990ec49bf1cb8158937b6aab91f6925c3285
Daniel Reszka <dannn.r@gmail.com>
2026-01-25 20:53:59 +0100

    commentary: add

---
+++ lua/custom/plugins/commentary.lua
@@ -0,0 +1,8 @@
+-- Comment stuff out. 
+-- `gcc` to comment out a line (takes a count), 
+-- `gc` to comment out the target of a motion 
+--   * for example, `gcap `to comment out a paragraph
+
+return {
+  'tpope/vim-commentary',
+}
