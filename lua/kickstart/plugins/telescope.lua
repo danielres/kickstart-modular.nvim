5ff915e66ee6275dacc8f2d4fc0824781c86de20
Daniel Reszka <dannn.r@gmail.com>
2026-01-25 08:16:12 +0100

    keymaps: minor cleanup

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -103,7 +103,7 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Find existing [B]uffers' })
-      vim.keymap.set('n', '<C-Space>', builtin.buffers, { desc = 'Find existing [B]uffers' })
+      vim.keymap.set('n', '<C-Space>', builtin.buffers, { desc = 'Find existing Buffers' })
       vim.keymap.set('n', '<leader>sc', builtin.command_history, { desc = '[C]ommand history' })
       vim.keymap.set('n', '<leader>sp', "<cmd>Telescope projects<cr>", { desc = '[P]rojects' })
 
