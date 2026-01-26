4be6b19de3eeaa3af67c45982bbbdb0d3f191d4b
Daniel Reszka <dannn.r@gmail.com>
2026-01-26 23:19:18 +0100

    vim-cutlass: add + setup

---
+++ lua/custom/plugins/cutlass.lua
@@ -0,0 +1,12 @@
+-- Prevents deletion from writing into registers
+-- 'm' is now used for 'move' (cut)
+
+return {
+  'svermeulen/vim-cutlass',
+  config = function()
+    vim.keymap.set('n', 'm', 'd',   { noremap = true, desc = '[M]ove (cut)' })
+    vim.keymap.set('x', 'm', 'd',   { noremap = true, desc = '[M]ove (cut)' })
+    vim.keymap.set('n', 'mm', 'dd', { noremap = true, desc = '[M]ove (cut) line ' })
+    vim.keymap.set('n', 'M', 'D',   { noremap = true, desc = '[M]ove (cut) to eol' })
+  end,
+}
