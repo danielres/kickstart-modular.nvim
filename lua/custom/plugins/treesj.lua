84a1a36e290dcefa718b22e662ec731a554752e3
Daniel Reszka <dannn.r@gmail.com>
2026-01-26 16:24:26 +0100

    TSJoin: simplify keymaps

--- lua/custom/plugins/treesj.lua
+++ lua/custom/plugins/treesj.lua
@@ -1,9 +1,9 @@
 return {
   'Wansmer/treesj',
   keys = {
-    { '<leader>ct', '<cmd>TSJToggle<CR>', desc = 'TS Join: Toggle' },
-    { '<leader>cj', '<cmd>TSJoin<CR>', desc = 'TS Join: Join' },
-    { '<leader>cs', '<cmd>TSSplit<CR>', desc = 'TS Join: Split' },
+    { '<leader>cj', '<cmd>TSJToggle<CR>', desc = 'TS[J]oin Toggle' },
+    -- { '<leader>cjj', '<cmd>TSJoin<CR>', desc = 'Join' },
+    -- { '<leader>cjs', '<cmd>TSSplit<CR>', desc = 'Split' },
   },
   dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
   config = function()
