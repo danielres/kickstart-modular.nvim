01f5cadb6bf465cdfb41f9a7abe758faf2926bb1
Daniel Reszka <dannn.r@gmail.com>
2026-01-28 01:22:31 +0100

    autocmd: set window title to cwd + filename

--- lua/custom/autocmd.lua
+++ lua/custom/autocmd.lua
@@ -10,3 +10,10 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.conceallevel = 2
   end,
 })
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'DirChanged' }, {
+  callback = function()
+    vim.opt.title = true
+    vim.opt.titlestring = vim.fn.fnamemodify(vim.fn.getcwd(), ':t') .. '        ' .. '%t'
+  end,
+})
