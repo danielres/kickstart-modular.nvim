1eae84428d1b8b3ff9ed759b5a4710ce527bdb15
Daniel Reszka <dannn.r@gmail.com>
2026-01-28 01:23:30 +0100

    use only 1 statusline for all windows

--- lua/options.lua
+++ lua/options.lua
@@ -11,6 +11,7 @@ vim.o.wrap = false
 vim.opt.foldenable = true
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
+vim.o.laststatus = 3
 -- /CUSTOM ======================================
 
 -- Make line numbers default
