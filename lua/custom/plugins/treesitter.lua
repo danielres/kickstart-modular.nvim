2c7234ed82cb9c330167deb83ad76c8c41c16c0f
Daniel Reszka <dannn.r@gmail.com>
2026-02-16 14:17:32 +0100

    treesitter.lua: fix config
    * fixes syntax highlighting issues with embedded svelte in elixir

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,27 +1,19 @@
-local languages = { 'c', 'lua', 'rust', 'elixir', 'heex', 'nix' }
-
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    branch = 'main',
-    build = ':TSUpdate',
-  },
-  {
-    'MeanderingProgrammer/treesitter-modules.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    opts = {
-      ensure_installed = languages,
-      fold = { enable = true },
-      highlight = { enable = true },
-      indent = { enable = true },
-      auto_install = true,
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<CR>',
-          node_incremental = '<CR>',
-          node_decremental = '<BS>',
-        },
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  main = 'nvim-treesitter.configs',
+  opts = {
+    ensure_installed = { 'c', 'lua', 'rust', 'elixir', 'heex', 'nix', 'svelte' },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<CR>',
+        node_incremental = '<CR>',
+        node_decremental = '<BS>',
+        scope_incremental = '<Tab>',
       },
     },
   },
